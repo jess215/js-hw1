22b008278d478f0f60fe200f41cf93ec5fa1b38c
Jessica Nguyen <jess@macbook-air.lan>
2022-02-15 17:07:27 -0700

    Completed more problems

--- homework1.js
+++ homework1.js
@@ -1,47 +1,47 @@
 // Loops
 
 // 1. Write a for loop that logs only logs even numbers from 0 - 10; ie 0,2,4,..10
-console.log('Count even numbers from 0')
-for (let i = 0; i <= 10; i += 2) {
-    console.log(i);
-}
-
+////console.log('Count even numbers from 0')
+////for (let i = 0; i <= 10; i += 2) {
+////    console.log(i);
+////}
+////
 // 2. Write a for that logs numbers from 10 - 0; ie 10,9,8...0
-console.log('Count down from 10')
-for (let i = 10; i >= 0; i--) {
-    console.log(i)
-}
-
+////console.log('Count down from 10')
+////for (let i = 10; i >= 0; i--) {
+////    console.log(i)
+////}
+////
 // 3. write a for loop that loops through this array and logs 'odd/even' based on wether the number is odd even
-console.log('Odd/Even label from 10')
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-nums.forEach((c)=>{
-    if (c % 2 ===0) {
-        console.log(`${c} even`);
-    } else {
-        console.log(`${c} odd`)
-    }
-})
-
+////console.log('Odd/Even label from 10')
+////let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+////
+////nums.forEach((c)=>{
+////    if (c % 2 ===0) {
+////        console.log(`${c} even`);
+////    } else {
+////        console.log(`${c} odd`)
+////    }
+////})
+////
 // 4. write a while loop that starts at 0 and counts up to 10 ie: 0,1,2,3....10
-console.log('Count to 10')
-for (let i = 1; i <= 10; i++) {
-    console.log(i)
-}
-
-
+////console.log('Count to 10')
+////for (let i = 1; i <= 10; i++) {
+////    console.log(i)
+////}
+////
+////
 // 5. Look closely at the code below
-const isOdd = (num) =>{
-    return num % 2 === 0
-}
-
-if(isOdd(2)){
-    console.log('is Odd')
-} else {
-    console.log('is Even')
-}
-
+////const isOdd = (num) =>{
+////    return num % 2 === 0
+////}
+////
+////if(isOdd(2)){
+////    console.log('is Odd')
+////} else {
+////    console.log('is Even')
+////}
+////
 // 6. what will this log?
 //// It will return the log "isOdd"
 
@@ -51,29 +51,48 @@ if(isOdd(2)){
 
 
 // 8. Fix the isOdd function to make this work
-const isEven = (num) =>{
-    if(num % 2 === 0){
-        return 'is Even'
-    }else{
-        return 'is Odd'
-    }
-}
-
-let x = isEven(2)
-console.log(x)
+////console.log('Returns even/odd')
+////const isEven = (num) =>{
+////    if(num % 2 === 0){
+////        return 'is Even'
+////    }else{
+////        return 'is Odd'
+////    }
+////}
+////
+////let x = isEven(2)
+////console.log(x)
 
 ////////////////
 
 // Functions
 
 // 1. Write a function that takes a positive number and prints all even numbers from 0 – number
-
+////console.log('Print all positive numbers in a range 0-27')
+////for(let i = 0; i <= 27; i += 2){
+////    console.log(i)
+////}
 
 // 2. Write a function that takes an array and returns the sum of all the numbers
-
+////console.log('Sum of an array of numbers')
+////function getSum(numArray){
+////    let sum = 0;
+////    for(let i = 0; i < numArray.length; i++) {
+////        sum += numArray[i];
+////    }
+////    console.log(sum)
+////}
+////getSum([2, 5, 6, 7, 9]);
 
 // 3. Write a function that returns the number of vowels in string
-
+//////////
+//console.log('Count number of vowels in a string')
+//
+//function getVowels(str) {
+//    var m = str.match(/[aeiou]/gi);
+//    return m === null ? 0 : m.length;
+//}
+//////////
 
 // 4. Write a function that takes a math operator, as a string, ('*','/','+','-') and two numbers and returns the result
 
